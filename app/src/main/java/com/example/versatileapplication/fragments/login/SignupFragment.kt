bd9c461a45c598e273977048b434808236c21923
NanthawatKTY <nanthawat.tae123@gmail.com>
2020-12-03 18:07:17 +0700

    Edit sign up fragment

--- app/src/main/java/com/example/versatileapplication/fragments/login/SignupFragment.kt
+++ app/src/main/java/com/example/versatileapplication/fragments/login/SignupFragment.kt
@@ -2,59 +2,48 @@ package com.example.versatileapplication.fragments.login
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.example.versatileapplication.R
+import kotlinx.android.synthetic.main.fragment_signup.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class SignupFragment : Fragment(R.layout.fragment_signup) {
+ // TODO: Change to onViewCreated
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SignupFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class SignupFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//
+//        // Create an ArrayAdapter
+//        val adapter = activity?.let {
+//            ArrayAdapter.createFromResource(it,
+//                R.array.question_lists, android.R.layout.simple_spinner_item)
+//        }
+//        // Specify the layout to use when the list of choices appears
+//        adapter?.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+//        // Apply the adapter to the spinner
+//        spinnerQuestion2.adapter = adapter
+//
+//    }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+//    fun getValues(view: View) {
+//        Toast.makeText(this, "Spinner 1 " + spinnerQuestion2.selectedItem.toString() +
+//                "\nSpinner 2 " + spinner2.selectedItem.toString(), Toast.LENGTH_LONG).show()
+//    }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signup, container, false)
-    }
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+////        super.onViewCreated(view, savedInstanceState)
+//
+//        // Create an Array Adapter
+//
+//        // Specify the layout to use when the list of choices appears
+//        context?.let { ArrayAdapter.createFromResource(it, R.array.question_lists, android.R.layout.simple_spinner_item ) }
+//            ?.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+//
+//        // Apply the adapter to the spinner
+//        spinnerQuestion2.adapter = adapter
+//
+//    }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SignupFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SignupFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
